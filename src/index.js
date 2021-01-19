c5ce2aa74143a8c484d4d2f544e46c623d6d9712
ZamiYizo21 <71885428+ZamiYizo21@users.noreply.github.com>
2021-01-19 09:58:56 -0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-alert */
+/* eslint-disable no-console */
+// Script de Frontend
+// importar los estilos
+// import './styles/styles.css';
+import 'materialize-css/dist/css/materialize.css';
+import 'materialize-css/dist/js/materialize';
+import initDropdown from './helpers/dropdowninit';
+
+initDropdown();
+// console.log('Modern JS Working!!! Thanks Babel...');
+// const show = (message = 'Saludo por defecto') => {
+//  alert(message);
+// };
