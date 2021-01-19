c7a65b8cb808c0c01a34f724da3cc9f5dfb4ac58
ZamiYizo21 <71885428+ZamiYizo21@users.noreply.github.com>
2021-01-19 09:55:40 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# diawZamudio-2020-02
+
+## Description
+Codigo base para administrar paqueteria front end.
+
+##  Stack
+Por definir 
+
+## Configuraciones Inciciales
+-'"npm config list"'
+Estas son configuraciones iniciales de npm
+- Configura en nombre del autor
+'$ npm set init.author.name "Jose Zamudio"'
+- Configura el correo electronico
+'$ npm set init.author.email "josedejesus.zm@gmail.com"'
+- Url del Author
+'$ npm set init.author.url "jose.com"'
+- URL de Licencia
+'$ npm set init.license "CC0"'
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const ESlintPlugin = require('eslint-webpack-plugin')
+
+
+module.exports = {
+    // 1. Especificando el archivo index de entrada
+    entry: path.join(__dirname, '/src/index.js') ,
+    // 2. Especificar el archivo de salida
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    // 3. Folder donde servira
+    devServer: {
+        contentBase: path.join(__dirname, './dist'),
+        port: 4040,
+        compress: true,
+        publicPath: '/'
+    },
+    module : {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use : ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use:['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins :[new ESlintPlugin()]
+}
